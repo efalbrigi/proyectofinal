32b663a4636871a2a7e06e4615faf8a7c422ebea
efalbrigi <efalbrigi@gmail.com>
2021-12-11 16:14:57 -0300

    varios

--- java/script.js
+++ java/script.js
@@ -1,4 +1,8 @@
-$('ul li').on('click', function() {
-	$('li').removeClass('active');
-	$(this).addClass('active');
-});
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
